254bedaec44d17ce1b7d510337237e04844643d3
Kei <71589461+keiwsh@users.noreply.github.com>
2024-09-20 18:45:16 +0200

    fix astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { remarkReadingTime } from "./remark-reading-time.mjs";
 import node from "@astrojs/node";
 
 export default defineConfig({
-  site: "https://hoangkhangle.com", // Your GitHub Pages base URL// The repository name
+  site: "https://hoangkhangle.ch", // Your GitHub Pages base URL// The repository name
   integrations: [tailwind(), icon()],
   output: "static",
   adapter: node({
