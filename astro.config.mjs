e3b434b7af3568f2bfc805edad1ac2a004d9f8e6
Kei <71589461+keiwsh@users.noreply.github.com>
2024-09-20 18:32:23 +0200

    Save changes before switching to gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import icon from 'astro-icon';
-import { remarkReadingTime } from './remark-reading-time.mjs';
-import node from '@astrojs/node';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+import { remarkReadingTime } from "./remark-reading-time.mjs";
+import node from "@astrojs/node";
 
 export default defineConfig({
-  site: 'https://keiwsh.github.io',  // Your GitHub Pages base URL
-  base: '/hoangkhangle.ch/',  // The repository name
+  site: "https://keiwsh.github.io", // Your GitHub Pages base URL
+  base: "/hoangkhangle.ch/", // The repository name
   integrations: [tailwind(), icon()],
-  output: 'server',
+  output: "static",
   adapter: node({
-    mode: 'standalone',
+    mode: "standalone",
   }),
   markdown: {
     remarkPlugins: [remarkReadingTime],
